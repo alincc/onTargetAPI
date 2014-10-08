ec9debb16560ec7896554802714f313737553d40
Sanjeev Ghimire <gsanjeev7@gmail.com>
2014-10-08 19:20:23 -0400

    java api for ontarget cms tool.

---
+++ src/main/java/com/ontarget/api/rs/Hello.java
@@ -0,0 +1,22 @@
+
+package com.ontarget.api.rs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/** Example resource class hosted at the URI path "/hello"
+ */
+@Path("/hello")
+public class Hello {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String hello() {
+        return "Hi there, This is OnTarget - Construction Management Tool.";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>OnTarget RESTful Web Application!</h2>
+<p><a href="services/hello">OnTarget restful web services</a></p>
+</body>
+</html>
