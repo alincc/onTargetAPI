084fa87252bae3bf0ac9dad320b7e94e50c0d07c
Sanjeev Ghimire <gsanjeev7@gmail.com>
2015-01-24 20:17:52 -0500

    report service

--- src/main/java/com/ontarget/api/service/impl/ProjectReportServiceImpl.java
+++ src/main/java/com/ontarget/api/service/impl/ProjectReportServiceImpl.java
@@ -316,30 +316,30 @@ public class ProjectReportServiceImpl implements ProjectReportService {
              */
 
             double costPerformanceIndex = 0;
-            if (report.getCumulativeActualCost() == 0) {
+            if (report.getCumulativeActualCost() > 0) {
                 costPerformanceIndex = report.getCumulativeEarnedValue() / report.getCumulativeActualCost();
-                report.setCostPerformanceIndex(costPerformanceIndex);
             }
+            report.setCostPerformanceIndex(costPerformanceIndex);
 
             /**
              * schedule performance index.
              */
-            double schedulePerformanceIndex=0;
-            if (report.getCumulativePlannedValue() == 0) {
+            double schedulePerformanceIndex = 0;
+            if (report.getCumulativePlannedValue() > 0) {
                 schedulePerformanceIndex = report.getCumulativeEarnedValue() / report.getCumulativePlannedValue();
-                report.setCostPerformanceIndex(schedulePerformanceIndex);
             }
+            report.setCostPerformanceIndex(schedulePerformanceIndex);
 
 
             /**
              * calculate estimated cost at completion.
              *
              */
-            double estimatedCostAtCompletion=0;
-            if(costPerformanceIndex==0) {
+            double estimatedCostAtCompletion = 0;
+            if(costPerformanceIndex > 0) {
                 estimatedCostAtCompletion = totalBudgetedCost * (1 / costPerformanceIndex);
-                report.setEstimatedCostAtCompletion(estimatedCostAtCompletion);
             }
+            report.setEstimatedCostAtCompletion(estimatedCostAtCompletion);
 
         }
 
