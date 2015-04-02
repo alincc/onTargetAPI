de166b4ff1c4a0a35669f031b106a54c5a6407b2
Sanjeev Ghimire <gsanjeev7@gmail.com>
2015-04-01 21:13:44 -0400

    logger info

--- src/main/java/com/ontarget/api/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ontarget/api/service/impl/AuthenticationServiceImpl.java
@@ -26,7 +26,7 @@ import java.util.List;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-	private Logger logger = Logger.getLogger(UserProfileImpl.class);
+	private Logger logger = Logger.getLogger(AuthenticationServiceImpl.class);
 
 	@Autowired
 	private AuthenticationDAO authenticationDAO;
