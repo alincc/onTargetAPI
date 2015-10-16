94bec41dd27b5cc5e80d755e719b5740d766a9af
Sanjeev Ghimire <gsanjeev7@gmail.com>
2015-10-16 14:53:39 -0700

    remove base request from user profile request

--- src/main/java/com/ontarget/request/bean/UserProfileRequest.java
+++ src/main/java/com/ontarget/request/bean/UserProfileRequest.java
@@ -1,16 +1,13 @@
 package com.ontarget.request.bean;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class UserProfileRequest {
-	@NotNull
-	@Valid
-	private BaseRequest baseRequest;
-	@NotNull
+
+    @NotNull
 	private Integer userId;
 
 }
